ed4894e3e1721333db9fc6438732436bced85165
rhysgoehring <rhysgoehring@gmail.com>
2017-06-22 11:45:17 -0600

    scaffolding out signIn

--- src/auth/signIn.js
+++ src/auth/signIn.js
@@ -1,13 +1,22 @@
 import React, {Component} from 'react';
+import {reduxForm} from 'redux-form';
 
 class SignIn extends Component {
   render() {
     return (
-      <div>
-        <h1>SignIn</h1>
-      </div>
+      <form>
+        <fieldset className="form-group">
+          <label>Username</label>
+          <input className="form-control"/>
+        </fieldset>
+        <fieldset className="form-group">
+          <label>Password:</label>
+          <input type="password" className="form-control"/>
+        </fieldset>
+        <button action="submit" className="btn btn-primary">Sign in</button>
+      </form>
     )
   }
 }
 
-export default SignIn;
+export default  SignIn;
